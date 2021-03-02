39df7b9e3ed4763bbbd333969b9db6db0a2161df
Alberto Medeiros <b3tomedeiros@gmail.com>
2021-03-02 19:26:23 -0300

    Commit de alteração do arquivo recebedor.php

--- recebedor.php
+++ recebedor.php
@@ -1,6 +1,8 @@
 <?php
+
 	session_start();
 	
+
 	$nome = filter_input(INPUT_POST, 'nome', FILTER_SANITIZE_SPECIAL_CHARS);
 	$email = filter_input(INPUT_POST, 'email', FILTER_VALIDATE_EMAIL);
 	$idade = filter_input(INPUT_POST, 'idade', FILTER_VALIDATE_INT);
